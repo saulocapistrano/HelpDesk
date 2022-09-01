0affd4d23c29b796d03d675c1b82d162b635bbc7
Matheus Abreu <matheus.abreu.magalhaes@gmail.com>
2022-09-01 16:07:37 -0300

    Update para chamado implementado

--- src/main/java/com/sjnc/HelpDesk/services/ChamadoService.java
+++ src/main/java/com/sjnc/HelpDesk/services/ChamadoService.java
@@ -1,5 +1,6 @@
 package com.sjnc.HelpDesk.services;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -40,6 +41,12 @@ public class ChamadoService {
 	public Chamado create(@Valid ChamadoDTO objDTO) {
 		return repository.save(newChamado(objDTO));
 	}
+	public Chamado update(Integer id, @Valid ChamadoDTO objDTO) {
+		objDTO.setId(id);
+		Chamado oldObj = findById(id);
+		oldObj = newChamado(objDTO);
+		return repository.save(oldObj);
+	}
 	
 	private Chamado newChamado(ChamadoDTO obj) {
 		Tecnico tecnico = tecnicoService.findById(obj.getTecnico());
@@ -49,6 +56,10 @@ public class ChamadoService {
 		if(obj.getId()!=null) {
 			chamado.setId(obj.getId());
 		}
+		
+		if(obj.getStatus().equals(2)) {
+			chamado.setDataFechamento(LocalDate.now());
+		}
 		chamado.setTecnico(tecnico);
 		chamado.setCliente(cliente);
 		chamado.setPrioridade(Prioridade.toEnum(obj.getPrioridade()));
@@ -56,4 +67,6 @@ public class ChamadoService {
 		chamado.setObsercavoes(obj.getObsercavoes());
 		return chamado;
 	}
+
+	
 }
