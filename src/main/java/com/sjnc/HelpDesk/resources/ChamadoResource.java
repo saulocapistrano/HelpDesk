25413ff1cb2a0e2b34d720ce3ecdb8e0697be6d6
Matheus Abreu <matheus.abreu.magalhaes@gmail.com>
2022-09-01 15:07:26 -0300

    criado o findById para Chamado

---
+++ src/main/java/com/sjnc/HelpDesk/resources/ChamadoResource.java
@@ -0,0 +1,28 @@
+package com.sjnc.HelpDesk.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sjnc.HelpDesk.domain.Chamado;
+import com.sjnc.HelpDesk.domain.dtos.ChamadoDTO;
+import com.sjnc.HelpDesk.services.ChamadoService;
+
+@RestController
+@RequestMapping(value = "/chamados")
+public class ChamadoResource {
+	
+	@Autowired
+	private ChamadoService service;
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<ChamadoDTO> findById(@PathVariable Integer id){
+		Chamado obj = service.findById(id);
+	return ResponseEntity.ok().body(new ChamadoDTO(obj));	
+	}
+}
+
+
---
+++ src/main/java/com/sjnc/HelpDesk/services/ChamadoService.java
@@ -0,0 +1,21 @@
+package com.sjnc.HelpDesk.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sjnc.HelpDesk.domain.Chamado;
+import com.sjnc.HelpDesk.repositories.ChamadoRepository;
+import com.sjnc.HelpDesk.services.exceptions.ObjectnotFoundException;
+
+@Service
+public class ChamadoService {
+	@Autowired
+	private ChamadoRepository repository;
+	
+	public Chamado findById(Integer id) {
+		Optional<Chamado> obj = repository.findById(id);
+		return obj.orElseThrow(()-> new ObjectnotFoundException("Objeto não encontrado Id: "+id));
+	}
+}
