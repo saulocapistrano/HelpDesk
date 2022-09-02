e7a4d99300514105aeba2882754124a70e13fc85
Matheus Abreu <matheus.abreu.magalhaes@gmail.com>
2022-09-02 14:43:57 -0300

    parcial da autorização

--- src/main/java/com/sjnc/HelpDesk/security/JWTUtil.java
+++ src/main/java/com/sjnc/HelpDesk/security/JWTUtil.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
@@ -22,5 +23,33 @@ public class JWTUtil {
 				.setExpiration(new Date(System.currentTimeMillis()+expiration))
 				.signWith(SignatureAlgorithm.HS512, secret.getBytes())
 				.compact();
+	}
+
+	public boolean tokenValido(String token) {
+		Claims claims = getClaims(token);
+		if(claims !=null) {
+			String username = claims.getSubject();
+			Date expirationsDate = claims.getExpiration();
+			Date now = new Date(System.currentTimeMillis());
+			
+			if(username !=null && expirationDate != null && now.before(expirationsDate)) {
+			return true;			
+			}
+		}
+		return false;
+	}
+
+	private Claims getClaims(String token) {
+		try {
+			return Jwts.parser().setSigningKey(secret.getBytes()).parseClaimsJws(token).getBody();
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		};
+	}
+
+	public String getUsername(String token) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
